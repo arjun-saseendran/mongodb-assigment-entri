af326e543b7503264551238f1a1e0bb4cb409e82
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-22 14:46:51 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ServerRouter from "./routes/server.routes.js";
 import dbConnection from "./db/db.connection.js"
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 dotenv.config({ path: "./.env" });
 
 // middlewares
